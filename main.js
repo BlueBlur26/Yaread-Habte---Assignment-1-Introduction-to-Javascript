ae7b0b33be5ebe296ec3dc89f3fa5eeb7ceba780
BlueBlur26 <64575361+BlueBlur26@users.noreply.github.com>
2023-04-05 19:48:12 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+document.write("Hello World!")
+
+alert("Hello everyone, welcome to my website!")
+
+
+let Studentname=prompt("What is your student name?");
+console.log,("Greetings", +  Studentname);
+alert('Greetings! Yaread Habte, Welcome to your profile!');
+
+
+document.getElementById ("learnbutton").addEventListener("click",function() {
+    document.getElementById ("passion-text").style.display="block";});
